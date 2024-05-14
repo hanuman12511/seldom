4d555b78fd1bc19197cd13a1dcc36a7f0a8a7586
hanuman12511 <er.hanumansaini@gmail.com>
2024-05-14 17:11:56 +0530

    update

--- src/data/store/Api/index.js
+++ src/data/store/Api/index.js
@@ -20,3 +20,57 @@ export const fetchUser=async()=>{
     .catch(error=>reject(error))
   })
 }
+
+
+
+export const BASE_URL = '';
+
+/* const AXIOS = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'content-type': 'multipart/form-data',
+  },
+});
+ */
+// Methods
+export const makeRequest = async (
+  url,
+  params = null,
+) => {
+  return new Promise(async(resolve,reject)=>{
+
+    let info = {};
+    info.url = url;
+    if (params) {
+      info.method = 'POST';
+      info.headers={ 
+        'content-type': 'multipart/form-data',
+      }
+      const formData = new FormData();
+      for (const key in params) {
+        if (key === 'images' && Array.isArray(params[key])) {
+          for (const Mem of Object.keys(params[key])) {
+            formData.append(key + '[]', params[key][Mem]);
+          }
+        } else {
+          formData.append(key, params[key]);
+        }
+      }
+      info.data = formData;
+    
+      }
+      else{
+        info.method="get";
+        info.headers= {
+          "Content-Type": "application/json"
+        }}
+
+        console.log(info);
+        fetch("https://jsonplaceholder.typicode.com/users",{info}).then(res=>{
+          resolve(res)
+        }).catch(error=>{
+          reject(error)
+        })
+  })
+
+};
--- src/screens/SearchLeads.tsx
+++ src/screens/SearchLeads.tsx
@@ -1,9 +1,236 @@
-import React from 'react'
-import {Text} from 'react-native'
-export default function SearchLeads(){
-    return(
-        <Text>
-            SearchLeads
-        </Text>
-    )
-}
+
+import React, {useState} from 'react';
+// Import required components
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Platform,
+  PermissionsAndroid,
+  Alert,
+} from 'react-native';
+
+// Import Image Picker
+import ImagePicker from 'react-native-image-picker';
+import {
+  launchCamera,
+  launchImageLibrary
+} from 'react-native-image-picker';
+
+const SearchLeads= () => {
+  const [filePath, setFilePath] = useState({});
+
+  const requestCameraPermission = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          {
+            title: 'Camera Permission',
+            message: 'App needs camera permission',
+          },
+        );
+        // If CAMERA Permission is granted
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } catch (err) {
+        console.warn(err);
+        return false;
+      }
+    } else return true;
+  };
+
+  const requestExternalWritePermission = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'External Storage Write Permission',
+            message: 'App needs write permission',
+          },
+        );
+        // If WRITE_EXTERNAL_STORAGE Permission is granted
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } catch (err) {
+        console.warn(err);
+        Alert.alert('Write permission err', err);
+      }
+      return false;
+    } else return true;
+  };
+
+  const captureImage = async (type:any) => {
+    let options = {
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+      videoQuality: 'low',
+      durationLimit: 30, //Video max duration in seconds
+      saveToPhotos: true,
+      base64: true
+    };
+    let isCameraPermitted = await requestCameraPermission();
+    let isStoragePermitted = await requestExternalWritePermission();
+    if (isCameraPermitted && isStoragePermitted) {
+      launchCamera(options, (response) => {
+        console.log('Response = ', response);
+
+        if (response.didCancel) {
+          Alert.alert('User cancelled camera picker');
+          return;
+        } else if (response.errorCode == 'camera_unavailable') {
+          Alert.alert('Camera not available on device');
+          return;
+        } else if (response.errorCode == 'permission') {
+          Alert.alert('Permission not satisfied');
+          return;
+        } else if (response.errorCode == 'others') {
+          Alert.alert(response.errorMessage);
+          return;
+        }
+        const {assets} =response
+        console.log(assets);
+        
+        console.log('base64 -> ', assets[0]?.base64);
+        console.log('uri -> ', assets[0]?.uri);
+        console.log('width -> ', assets[0]?.width);
+        console.log('height -> ', assets[0]?.height);
+        console.log('fileSize -> ', assets[0]?.fileSize);
+        console.log('type -> ', assets[0]?.type);
+        console.log('fileName -> ', assets[0]?.fileName);
+        setFilePath(assets[0]);
+      });
+    }
+  };
+
+  const chooseFile = (type) => {
+    let options = {
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+  
+      includeBase64: true
+    };
+    console.log("8888888888888888888888888888888888");
+    
+  /*   ImagePicker.showImagePicker(options, (response) => {
+      const base64Value = response.data;
+    }); */
+
+    launchImageLibrary(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        Alert.alert('User cancelled camera picker');
+        return;
+      } else if (response.errorCode == 'camera_unavailable') {
+        Alert.alert('Camera not available on device');
+        return;
+      } else if (response.errorCode == 'permission') {
+        Alert.alert('Permission not satisfied');
+        return;
+      } else if (response.errorCode == 'others') {
+        Alert.alert(response.errorMessage);
+        return;
+      }
+      const {assets} =response
+      console.log(assets);
+      console.log('base64 -> ', response.base64);
+      console.log('uri -> ', response.uri);
+      console.log('width -> ', response.width);
+      console.log('height -> ', response.height);
+      console.log('fileSize -> ', response.fileSize);
+      console.log('type -> ', response.type);
+      console.log('fileName -> ', response.fileName);
+      setFilePath(assets[0]);
+    });
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Text style={styles.titleText}>
+        Example of Image Picker in React Native
+      </Text>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: 'data:image/jpeg;base64,' + filePath.base64,
+          }}
+          style={styles.imageStyle}
+        />  
+      {/*   <Image
+          source={{uri: filePath.uri}}
+          style={styles.imageStyle}
+        /> */}
+        <Text style={styles.textStyle}>{filePath.uri}</Text>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.buttonStyle}
+          onPress={() => captureImage('photo')}>
+          <Text style={styles.textStyle}>
+            Launch Camera for Image
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.buttonStyle}
+          onPress={() => captureImage('video')}>
+          <Text style={styles.textStyle}>
+            Launch Camera for Video
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.buttonStyle}
+          onPress={() => chooseFile('photo')}>
+          <Text style={styles.textStyle}>Choose Image</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.buttonStyle}
+          onPress={() => chooseFile('video')}>
+          <Text style={styles.textStyle}>Choose Video</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default SearchLeads;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+  textStyle: {
+    padding: 10,
+    color: 'black',
+    textAlign: 'center',
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 5,
+    marginVertical: 10,
+    width: 250,
+  },
+  imageStyle: {
+    width: 200,
+    height: 200,
+    margin: 5,
+  },
+});
